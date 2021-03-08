65b86062ce471c73d6a3e3089eb00d085f69fd36
Natalija-J <77069086+Natalija-J@users.noreply.github.com>
2021-03-08 19:01:27 +0200

    Add project files.

---
+++ Library/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using Library_Logic;
+namespace Library_Console
+{
+    public class Program
+    {
+        static BookManager manager = new BookManager();
+        static void Main(string[] args)
+        {
+            manager.BackgroundColor();
+            manager.ForegroundColor();
+            Console.WriteLine("Welcome to the library!");
+            Console.WriteLine("Books available:");
+            Console.ResetColor();
+            Console.WriteLine();
+
+            var books = manager.GetAvailableBooks();
+            if (books.Count == 0)
+            {
+                Console.WriteLine("There are no available books at the moment.");
+            }
+            else
+            {
+                books.ForEach(book =>
+                {
+                    Console.WriteLine("{0} ({1}) {2} (Available copies: {3})", book.Title, book.Author, book.Year, book.Copies);
+                });
+            }
+            Console.WriteLine();
+            while (true)
+            {
+                Console.WriteLine("Please enter a title of a book you wish to borrow " +
+                    "(or 'stop' to quit): ");
+                string input = System.Console.ReadLine();
+
+                if (input == "stop" || input == "STOP" || input == "Stop")
+                {
+                    break;
+                }
+
+                var bookBorrowed = manager.TakeBook(input);
+                if (bookBorrowed == null)
+                {
+                    System.Console.WriteLine("The book is not available!");
+                }
+                else
+                {
+                    Console.WriteLine("The book '{0}' was successfully checked out!", bookBorrowed.Title);
+                    Console.WriteLine();
+                }
+            }
+            Console.WriteLine();
+            manager.BackgroundColor();
+            manager.ForegroundColor();
+            Console.WriteLine("Your books: ");
+            Console.ResetColor();
+            var myList = manager.GetUserBooks();
+            if (myList.Count == 0)
+            {
+                Console.WriteLine("You have not checked out any books.");
+            }
+            else
+            {
+                foreach (var book in myList)
+                {
+                    Console.WriteLine("'{0}' by {1} ({2})",
+                        book.Title, book.Author, book.Year);
+                }
+                Console.WriteLine();
+                manager.BackgroundColor();
+                manager.ForegroundColor();
+                Console.WriteLine("Returning books.");
+                Console.ResetColor();
+
+                while (true)
+                {
+                    Console.WriteLine("Please enter a book title that you wish to return" +
+                        "(or 'stop' to exit): ");
+                    string input2 = System.Console.ReadLine();
+                    if (input2 == "stop" || input2 == "STOP" || input2 == "Stop")
+                    {
+                        break;
+                    }
+
+                    var returned = manager.ReturnBook(input2);
+                    if (returned == null)
+                    {
+                        System.Console.WriteLine("Sorry, this book has not been checked out by you!");
+                    }
+                    else
+                    {
+                        System.Console.WriteLine("'{0}' by {1} has been returned to the library (available copies: {3})",
+                        returned.Title, returned.Author, returned.Copies);
+                    }
+                }
+                System.Console.WriteLine();
+                manager.BackgroundColor();
+                manager.ForegroundColor();
+                System.Console.WriteLine("Books that have not been returned yet: ");
+                System.Console.ResetColor();
+
+                foreach (var book in manager.GetUserBooks())
+                {
+                    System.Console.WriteLine("'{0}' by {1} ({2})",
+                        book.Title, book.Author, book.Year);
+                }
+
+            }
+            System.Console.WriteLine();
+            manager.BackgroundColor();
+            manager.ForegroundColor();
+            System.Console.Write("Thank you for using our library!");
+            System.Console.ResetColor();
+        }
+    }
+}
---
+++ Library_Logic/Logic/BookManager.cs
@@ -0,0 +1,96 @@
+﻿using Library_Logic.DB;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+
+namespace Library_Logic
+{
+    /// <summary>
+    /// All the logic for taking/returning books
+    /// </summary>
+    public class BookManager
+    {       
+        public BookManager()
+        {
+        }
+        /// <summary>
+        /// Returns list of all available books
+        /// </summary>
+        /// <returns></returns>
+        public List<Books> GetAvailableBooks()
+        {
+            //    //return LibraryBooks.AvBooks
+            //    //                 .Where(book => book.Copies > 0)
+            //    //                 .OrderBy(book => book.Title)
+            //    //                 .ToList();
+            //    List<Book> result = new List<Book>();
+            //    string cs = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Stephen\Documents\Natalija\Accenture .Net\Bootcamp1\LibraryDB.mdf;Integrated Security=True;Connect Timeout=30";
+
+
+            using (LibraryDatabase db = new LibraryDatabase())
+            {
+                return db.Books.Where(b => b.Copies > 0)
+                                      .OrderBy(b => b.Title).ToList();                
+            }
+        }
+
+        public List<UserBooks> GetUserBooks()
+        {
+            using (LibraryDatabase db = new LibraryDatabase())
+            {
+                return db.UserBooks.OrderBy(b => b.Title).ToList();
+            }
+            //return UserBooks.Books.OrderBy(book => book.Title).ToList();
+        }
+
+       public Books TakeBook(string title)
+       {
+           //Book book = LibraryBooks.AvBooks.Find(book =>
+           //book.Title.Equals(title, StringComparison.OrdinalIgnoreCase));
+           using(LibraryDatabase db = new LibraryDatabase())
+           {
+                var book = db.Books.FirstOrDefault(b => b.Title.ToLower() == title.ToLower());
+
+                if (book != null && book.Copies > 0)
+                {
+                    book.Copies--;
+                    db.UserBooks.Add(new UserBooks()
+                    {
+                        Author = book.Author,
+                        Title = book.Title,
+                        Year = book.Year
+                    });
+                    
+                    db.SaveChanges();
+                    return book;
+                }               
+           }
+            return null;            
+       }
+        public Books ReturnBook(string title)
+        {
+            using(LibraryDatabase db = new LibraryDatabase())
+            {
+                var book = db.UserBooks.FirstOrDefault(b => b.Title.ToLower() == title.ToLower());
+                if(book != null)
+                {
+                    db.UserBooks.Remove(book);
+                    var returnedBook = db.Books.FirstOrDefault(b => b.Title.ToLower() == title.ToLower());
+                    returnedBook.Copies++;
+                    db.SaveChanges();
+                    return returnedBook;
+                }
+            }
+            return null;           
+        }
+        public void BackgroundColor()
+        {
+            Console.BackgroundColor = ConsoleColor.Green;
+        }
+        public void ForegroundColor()
+        {
+            Console.ForegroundColor = ConsoleColor.Black;
+        }
+    }
+}
